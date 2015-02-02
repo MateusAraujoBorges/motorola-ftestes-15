8432367d1960c8218ab3caf4f23c4b0c9611924b
Mateus Borges <mateusaraujoborges@cin.ufpe.br>
2015-02-01 22:37:10 -0300

    finishing test-execution examples

--- test-execution/texasholdem/src/test/java/ex5/DeckServiceTest.java
+++ test-execution/texasholdem/src/test/java/ex5/DeckServiceTest.java
@@ -1,8 +1,6 @@
-package ex3;
+package ex5;
 
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 
 import org.junit.Assert;
@@ -11,15 +9,10 @@ import org.ozsoft.texasholdem.Card;
 import org.ozsoft.texasholdem.Deck;
 
 public class DeckServiceTest {
-	
+
 	public static Deck getShuffledDeckInAVeryExpensiveWay() {
 		Deck deck = new Deck();
 		deck.shuffle();
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			System.out.println("Shouldn't happen");
-		}
 		return deck;
 	}
 	
@@ -27,27 +20,18 @@ public class DeckServiceTest {
 		output.append(deck.getNextCardIndex() + " " + deck.toString() + "\n");
 	}
 	
-    @Test
-    public void testCardsInAShuffledDeckAreNotEqual() {
+    @Test(timeout=6000)
+    public void testCardsInAShuffledDeckAreNotEqual() throws InterruptedException {
     	Deck d = getShuffledDeckInAVeryExpensiveWay();
     	Card c = d.deal();
     	
     	for (int i = 0; i < 50; i++) {
     		Assert.assertNotEquals(c, d.deal());
-    	}
-    	
-    	try {
-    		File file = new File("test-ex3.txt");
-    		BufferedWriter output = new BufferedWriter(new FileWriter(file,true));
-    		logDeckStateToFile(d, output);
-    		output.close();
-    	} catch (IOException e) {
-    		e.printStackTrace();
-    		Assert.fail("Error while writing file");
+    		Thread.sleep(100);
     	}
     }
     
-    @Test
+    @Test(timeout=100)
     public void testDrawingFiveCards() {
     	Deck d = getShuffledDeckInAVeryExpensiveWay();
     	
@@ -56,15 +40,5 @@ public class DeckServiceTest {
     	}
     	
     	Assert.assertEquals(5,d.getNextCardIndex());
-    	
-    	try {
-    		File file = new File("test-ex3.txt");
-    		BufferedWriter output = new BufferedWriter(new FileWriter(file,true));
-    		logDeckStateToFile(d, output);
-    		output.close();
-    	} catch (IOException e) {
-    		e.printStackTrace();
-    		Assert.fail("Error while writing file");
-    	}
     }    
 }
