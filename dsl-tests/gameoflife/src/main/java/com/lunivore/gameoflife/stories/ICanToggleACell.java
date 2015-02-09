2ad5e6289cc359ce999eb17fd341d555020a9a79
Mateus Borges <mateusaraujoborges@cin.ufpe.br>
2015-02-08 23:54:29 -0300

    better jbehave example

---
+++ dsl-tests/gameoflife/src/main/java/com/lunivore/gameoflife/stories/ICanToggleACell.java
@@ -0,0 +1,10 @@
+package com.lunivore.gameoflife.stories;
+
+import com.lunivore.gameoflife.GridStory;
+
+//maps to i_can_toggle_a_cell.story
+//can be executed as a junit test
+
+public class ICanToggleACell extends GridStory {
+
+}
---
+++ dsl-tests/gameoflife/src/main/java/com/lunivore/gameoflife/stories/TheGridStartsEmpty.java
@@ -0,0 +1,10 @@
+package com.lunivore.gameoflife.stories;
+
+import com.lunivore.gameoflife.GridStory;
+
+//maps to the_grid_starts_empty.story
+//can be executed as a junit test
+
+public class TheGridStartsEmpty extends GridStory {
+
+}
